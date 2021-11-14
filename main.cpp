da5b4da4218dcdff7a6129e552a593a7b8324b1d
D47ID3 <91840256+D47ID3@users.noreply.github.com>
2021-11-14 23:32:24 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   int n,i=1,b=1,a=0,c;
+int main()
+{
+   int n=0,i=1,b=1,a=0,c;
    cin>>n;
    
-   if(n>=2)
-   {
       cout<<b<<endl;
-      for(i=2,i<=n;i++);
+      for(i=0,i<n,i++);
       {
          c=a+b;
-         cout <<c<<endl;
+         cout<<c<<endl;
          a=b;
          b=c;
       }
-      
-   }
-   else
-   { 
-      cout<<"errore"<<endl;
-   }
-   
+ 
    return 0;
 }
